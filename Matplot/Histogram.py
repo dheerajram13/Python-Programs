9043d804adbfaaf267c8030fc13f940b2e02247e
Dheeraj srirama <rcharan10@gmail.com>
2017-06-21 15:27:59 +0530

    Delete Histogram.py

--- Matplot/Histogram.py
+++
@@ -1,8 +0,0 @@
-import random
-import numpy as np
-import matplotlib.pyplot as plt
-rolls = []
-for i in range(100):
-    rolls.append(random.choice([1,2,3,4,5,6]))
-plt.hist(rolls,bins = np.linspace(0.5,6.5,7))    
-plt.show()    
